28ae89c4eca1eb7323aa5c897ec54359bda2d266
Henry Nelson <Henry.Nelson89@gmail.com>
2020-05-22 09:24:10 -0700

    added the login endpint with genterating a token and added restiction functionallity to the get endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 const Users = require("../users/users-model");
 
 const { isValid } = require("./authenticate-middleware");
+const secret = require("./secret")
 
 router.post('/register', (req, res) => {
   const credentials = req.body;
@@ -29,7 +30,39 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  const { username, password } = req.body;
+
+  if (isValid(req.body)) {
+    Users.findBy({ username })
+      .then(([user]) => {
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          token = generateToken(user);
+
+          res.status(200).json({ message: `Welcome ${user.username}!! here is your token`, token })
+        } else {
+          res.status(401).json({ message: 'please provide a valid username and password' })
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: err.message })
+      })
+  } else {
+    res.status(400).json({ message: 'please provide a username and password, and the password should be alphanumeric' })
+  }
 });
+
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secret.jwtSecret, options)
+}
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,6 +2,8 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const secret = require("./secret")
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   isValid,
@@ -15,5 +17,18 @@ function isValid(user) {
 
 
 function restrict(req, res, next) {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization
+  if (token){
+    jwt.verify(token, secret.jwtSecret, (err, decodedToken) => {
+      if(err){
+        res.status(401).json({message: "invalid token"})
+      } else {
+        req.subject = decodedToken.subject
+        req.username = decodedToken.username
+        next()
+      }
+    })
+  } else {
+    res.status(401).json({ you: 'shall not pass!' });
+  }
 };
